62fc17999675da325fc59f72399890833c90656a
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2023-11-29 18:50:05 +0100

    megaCustomV1

--- API.js
+++ API.js
@@ -136,29 +136,48 @@ function generateCharts(dataArray) {
             // label: findCountryName(pays.zone),
             data: Object.values(countryData),
             backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
+              "rgba(0, 255, 0, 0.4)",
+              "rgba(255, 165, 0, 0.4)",
+              "rgba(139, 69, 19, 0.4)",
+              "rgba(0, 0, 0, 0.4)",
+              "rgba(255, 255, 255, 0.4)",
+              "rgba(255, 255, 0, 0.4)",
+              "rgba(0, 0, 255, 0.4)",
+              "rgba(128, 128, 128, 0.4)",
+              "rgba(139, 69, 19, 0.4)",
+
             ],
             borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
+              "rgba(0, 255, 0, 1)",
+              "rgba(255, 165, 0, 1)",
+              "rgba(139, 69, 19, 1)",
+              "rgba(0, 0, 0, 1)",
+              "rgba(255, 255, 255, 1)",
+              "rgba(255, 255, 0, 1)",
+              "rgba(0, 0, 255, 1)",
+              "rgba(128, 128, 128, 1)",
+              "rgba(139, 69, 19, 1)",
             ],
             borderWidth: 1,
           },
         ],
       },
       options: {
+        plugins: {  // 'legend' now within object 'plugins {}'
+          legend: {
+            labels: {
+              color: "white",
+            }
+          }
+        },
         scales: {
           y: {
             beginAtZero: true,
+            ticks: {
+              color: "white",  // not 'fontColor:' anymore
+              //fontSize: 14,
+
+            }
           },
         },
       },
